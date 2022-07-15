0f4603b19c2ba36d4ea81003e5cba47983c442a5
Mojahed <m.eshagh.m@gmail.com>
2022-07-15 13:16:02 +0430

    Make artilcelist component and fetch data from api and show them

---
+++ components/ArticleList.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+      <h1>Get article title</h1>
+      <div>
+        <ul>
+          <li v-for="article in articles" :key="article.id" class="item" >
+            <nuxt-link :to="'/' + article.id">{{ article.title }}</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props:{
+        articles: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
---
+++ pages/_id/index.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <h1>{{ articles.title }}</h1>
+        <p> {{ articles.body }}</p>
+    </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    async asyncData ({params}) {
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts/'+params.id)
+      return {articles:data}
+    }
+  }
+</script>
